ef552c80483a8029c2b57ba1605ea4a6d9055d49
efirlus <efirlus.laemas@gmail.com>
2020-03-12 10:12:20 +0900

    5회차 기준

--- repro/src/App.js
+++ repro/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'http://gangnamstar.co.kr/files/attach/images/119/324/028/0d19e13c06f3afd4bcb7eb92fb6247b5.jpg',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'http://gangnamstar.co.kr/files/attach/images/119/324/028/9529d54ec28c8be31fa5893e65a62c54.jpg',
+    'name': '나동빈',
+    'birthday': '960508',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'http://gangnamstar.co.kr/files/attach/images/119/324/028/46339009bb9c48a95dbbc34db8b4e706.jpg',
+    'name': '이순신',
+    'birthday': '961127',
+    'gender': '남자',
+    'job': '디자이너'
+  }
+]  
+  
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map(c => {
+          return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+        })}
+      </div>
+    );
+  }
 }
+  
 
 export default App;
